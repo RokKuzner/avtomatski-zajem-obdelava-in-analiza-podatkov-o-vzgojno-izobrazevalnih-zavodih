768861ab6e9629f6baebe038c49b187d02fa0c96
Rok Kuzner <rok.ankaran@gmail.com>
2025-10-29 13:42:23 +0100

    Refactor school data extraction to use a structured data list for improved maintainability

--- extract_basic_institution_info.py
+++ extract_basic_institution_info.py
@@ -6,6 +6,21 @@ import selenium
 primary_school_records_url = "https://paka3.mss.edus.si/registriweb/Seznam1.aspx?Seznam=2010"
 middle_school_records_url = "https://paka3.mss.edus.si/registriweb/Seznam2.aspx?Seznam=3010"
 
+data = [
+    {
+        "url": primary_school_records_url,
+        "name_c_n": "3",
+        "web_c_n": "10",
+        "label":"OS"
+    },
+    {
+        "url": middle_school_records_url,
+        "name_c_n": "1",
+        "web_c_n": "8",
+        "label":"SS"
+    }
+    ]
+
 # Setup driver for background use
 chrome_options = Options()
 chrome_options.add_argument("--headless=new")
@@ -18,13 +33,19 @@ chrome_options.add_argument("--window-size=1920,1080")
 driver = webdriver.Chrome(options=chrome_options)
 
 try:
-    # Primary school
-    driver.get(primary_school_records_url)
-    schools = driver.find_elements(By.CSS_SELECTOR, "#form1 > div:nth-child(19) > table > tbody > tr:nth-child(2) > td > table > tbody > tr")[1:]
-    for school in schools:
-        school_name = school.find_element(By.CSS_SELECTOR, ".celica:nth-child(3) > a").text
-        school_website = school.find_element(By.CSS_SELECTOR, ".celica:nth-child(10) > a").get_attribute("href")
-    
+    for datapoint in data:
+
+        driver.get(datapoint["url"])
+        rows = driver.find_elements(By.CSS_SELECTOR, "#form1 > div:nth-child(19) > table > tbody > tr:nth-child(2) > td > table > tbody > tr")
+        for row in rows:
+            try:
+                school_name = row.find_element(By.CSS_SELECTOR, f".celica:nth-child({datapoint["name_c_n"]}) > a").text
+                school_website = row.find_element(By.CSS_SELECTOR, f".celica:nth-child({datapoint["web_c_n"]}) > a").get_attribute("href")
+
+                print(datapoint["label"], school_name, school_website)
+            except:
+                continue
+
 except Exception as e:
     print("Exception:", e)
 finally:
