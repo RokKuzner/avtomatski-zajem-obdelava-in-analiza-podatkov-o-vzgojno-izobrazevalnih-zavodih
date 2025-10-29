02a38c0b6eb206d85584de10a453432187ba756b
Rok Kuzner <rok.ankaran@gmail.com>
2025-10-29 12:15:14 +0100

    Add regex for validating URLs in find_events_page.py

--- find_events_page.py
+++ find_events_page.py
@@ -3,8 +3,16 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 import requests
+import re
 
 load_dotenv()
+valid_url_regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 def find_events_page(base_url:str) -> str|None:
     url_to_check = base_url
