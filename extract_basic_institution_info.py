7d87033783247cc30d8e1a8cb507f41b3e63b442
Rok Kuzner <rok.ankaran@gmail.com>
2025-10-29 13:03:23 +0100

    Add URLs for primary and middle school records

---
+++ extract_basic_institution_info.py
@@ -0,0 +1,2 @@
+primary_school_records_url = "https://paka3.mss.edus.si/registriweb/Seznam1.aspx?Seznam=2010"
+middle_school_records_url = "https://paka3.mss.edus.si/registriweb/Seznam2.aspx?Seznam=3010"
