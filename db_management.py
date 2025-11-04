7525fd66b324eb772bf0a6c4cbab3227bbeaafff
Rok Kuzner <rok.ankaran@gmail.com>
2025-11-04 16:40:21 +0100

    Refactor db_management.py to encapsulate database connection logic within manage() function

--- db_management.py
+++ db_management.py
@@ -1,42 +1,43 @@
 import sqlite3
 
-connection = sqlite3.connect("database.db")
+def manage():
+    connection = sqlite3.connect("database.db")
 
-# Create a cursor object
-cursor = connection.cursor()
+    # Create a cursor object
+    cursor = connection.cursor()
 
-# Settings
-cursor.execute("""
-PRAGMA foreign_keys = ON;
-""")
+    # Settings
+    cursor.execute("""
+    PRAGMA foreign_keys = ON;
+    """)
 
-# Create the tables
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS vzgojno_izobrazevalni_zavodi (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    type TEXT,
-    name TEXT NOT NULL UNIQUE,
-    website TEXT,
-    municipality TEXT
-)
-""")
+    # Create the tables
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS vzgojno_izobrazevalni_zavodi (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        type TEXT,
+        name TEXT NOT NULL UNIQUE,
+        website TEXT,
+        municipality TEXT
+    )
+    """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS events_page_url (
-    id INTEGER PRIMARY KEY,
-    url TEXT,
-    FOREIGN KEY (id) REFERENCES vzgojno_izobrazevalni_zavodi(id)
-)
-""")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS events_page_url (
+        id INTEGER PRIMARY KEY,
+        url TEXT,
+        FOREIGN KEY (id) REFERENCES vzgojno_izobrazevalni_zavodi(id)
+    )
+    """)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS article_urls (
-    id INTEGER,
-    url TEXT,
-    FOREIGN KEY (id) REFERENCES vzgojno_izobrazevalni_zavodi(id)
-)
-""")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS article_urls (
+        id INTEGER,
+        url TEXT,
+        FOREIGN KEY (id) REFERENCES vzgojno_izobrazevalni_zavodi(id)
+    )
+    """)
 
-# Commit changes and close the connection
-connection.commit()
-connection.close()
+    # Commit changes and close the connection
+    connection.commit()
+    connection.close()
