4965ec068f9c194ade886fb205bd7d2684bbac7f
Rok Kuzner <rok.ankaran@gmail.com>
2025-11-06 16:10:18 +0100

    complete the google API calling function

--- extract_VIZ_webpage_article_texts.py
+++ extract_VIZ_webpage_article_texts.py
@@ -1,5 +1,11 @@
 import db_manipulation as db
 import requests
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def extract_texts():
     for viz in db.get_all_vzgojno_izobrazevalni_zavodi():
@@ -23,4 +29,53 @@ def extract_texts():
                 break
 
 def generate_extract_article_text_response(html:str) -> list[str]:
-    return []
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-2.5-flash-lite"
+    with open("./sys_instructions/extract_article_content.txt", "r") as f:
+        sys_instructions = f.read()
+
+    generate_content_config = types.GenerateContentConfig(
+        temperature=0.5,
+        thinking_config = types.ThinkingConfig(
+            thinking_budget=8192,
+        ),
+        response_mime_type="application/json",
+        response_schema=genai.types.Schema(
+            type = genai.types.Type.OBJECT,
+            required = ["heading", "content"],
+            properties = {
+                "heading": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+                "content": genai.types.Schema(
+                    type = genai.types.Type.STRING,
+                ),
+            },
+        ),
+        system_instruction=[
+            types.Part.from_text(text=sys_instructions)
+        ],
+    )
+
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text=f"HTML: {html}"),
+            ],
+        ),
+    ]
+
+    content = client.models.generate_content(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    )
+
+    return {
+        "heading": content.parsed["heading"],
+        "content": content.parsed["content"]
+    }
